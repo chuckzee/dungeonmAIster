844d70f10ed52b287fa75c107f11f3b5f09e31fa
Chuck <cezimmerman90@gmail.com>
2023-10-08 13:20:35 -0400

    Removed duplicative console logs

--- services/chatbot.js
+++ services/chatbot.js
@@ -36,9 +36,7 @@ const rest = new REST({ version: '10' }).setToken(TOKEN);
 
 async function registerCommands() {
   try {
-    console.log('Started refreshing application (/) commands.');
     await rest.put(Routes.applicationCommands(CLIENT_ID), { body: commands });
-    console.log('Successfully reloaded application (/) commands.');
   } catch (error) {
     console.error(error);
   }
